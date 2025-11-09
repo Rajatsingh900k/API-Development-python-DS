0ed7c26b041bb4ee5debd6022036d6b4094d9188
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-11-10 01:19:56 +0530

    Added second API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 import ipl
 app=Flask(__name__)
 
@@ -10,5 +10,12 @@ def home():
 def teams():
     teams=ipl.teamsApi()
     return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1=request.args.get('team1')
+    team2=request.args.get('team2')
+    temp=ipl.teamVteamApi(team1,team2)
+    return jsonify(temp)
 
 app.run(debug=True)
--- ipl.py
+++ ipl.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-
+import json
 matches=pd.read_csv("./Dataset/IPL_Matches_2008_2022.csv")
 
 
@@ -12,3 +12,18 @@ def teamsApi():
         'teams':teams
     }
     return teams_dict
+
+def teamVteamApi(team1,team2):
+    temp_df=matches[(matches['Team1']==team1) & (matches['Team2']==team2)|(matches['Team1']==team2) & (matches['Team2']==team1)]
+    total_matches=temp_df.shape[0]
+    matches_won_team1=temp_df['WinningTeam'].value_counts()[team1]
+    matches_won_team2=temp_df['WinningTeam'].value_counts()[team2]
+    draws=total_matches-(matches_won_team1+matches_won_team2)
+
+    response={
+        team1:str(matches_won_team1),
+        team2:str(matches_won_team2),
+        'total_matches':str(total_matches),
+        'draws':str(draws)
+    }
+    return response
