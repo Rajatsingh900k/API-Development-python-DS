c908095449048f886db86fd95a337fe57682a436
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-11-10 00:35:02 +0530

    configuring app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,jsonify
+import ipl
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/api/teams')
+def teams():
+    teams=ipl.teamsApi()
+    return jsonify(teams)
+
+app.run(debug=True)
