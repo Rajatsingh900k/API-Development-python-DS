5510e9f549b183f62fbe0e49a2b41062ca2d2753
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-11-10 01:37:12 +0530

    Completed

--- ipl.py
+++ ipl.py
@@ -1,29 +1,40 @@
-# writing logic for fetching
-
-import numpy as np
 import pandas as pd
-import json
-matches=pd.read_csv("./Dataset/IPL_Matches_2008_2022.csv")
+import numpy as np
 
+ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv"
+matches = pd.read_csv(ipl_matches)
 
-def teamsApi():
-    teams=list(set(list(matches['Team1'])+list(matches['Team2'])))
-    teams_dict={
+print(matches.head())
+
+def teamsAPI():
+    teams = list(set(list(matches['Team1']) + list(matches['Team2'])))
+    team_dict = {
         'teams':teams
     }
-    return teams_dict
 
-def teamVteamApi(team1,team2):
-    temp_df=matches[(matches['Team1']==team1) & (matches['Team2']==team2)|(matches['Team1']==team2) & (matches['Team2']==team1)]
-    total_matches=temp_df.shape[0]
-    matches_won_team1=temp_df['WinningTeam'].value_counts()[team1]
-    matches_won_team2=temp_df['WinningTeam'].value_counts()[team2]
-    draws=total_matches-(matches_won_team1+matches_won_team2)
+    return team_dict
 
-    response={
-        team1:str(matches_won_team1),
-        team2:str(matches_won_team2),
-        'total_matches':str(total_matches),
-        'draws':str(draws)
-    }
-    return response
+def teamVteamAPI(team1,team2):
+
+    valid_teams = list(set(list(matches['Team1']) + list(matches['Team2'])))
+
+    if team1 in valid_teams and team2 in valid_teams:
+
+        temp_df = matches[(matches['Team1'] == team1) & (matches['Team2'] == team2) | (matches['Team1'] == team2) & (matches['Team2'] == team1)]
+        total_matches = temp_df.shape[0]
+
+        matches_won_team1 = temp_df['WinningTeam'].value_counts()[team1]
+        matches_won_team2 = temp_df['WinningTeam'].value_counts()[team2]
+
+        draws = total_matches - (matches_won_team1 + matches_won_team2)
+
+        response = {
+              'total_matches': str(total_matches),
+              team1: str(matches_won_team1),
+              team2: str(matches_won_team2),
+              'draws': str(draws)
+          }
+
+        return response
+    else:
+        return {'message':'invalid team name'}
